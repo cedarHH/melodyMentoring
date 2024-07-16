591eebcafc39c1d58d957ffe7d1a3295e655aea3
Weiming Wu <z5434640@ad.unsw.edu.au>
2024-07-16 14:53:26 +1000

    fix animation error

--- mobile/App/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { expo as appConfig} from './app.json';
-
-AppRegistry.registerComponent(appConfig.name, () => App);
