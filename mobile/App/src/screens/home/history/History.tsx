ac530c71382bf65c142a4114108829e0260357ed
Ranran1iu <liuleobrown@gmail.com>
2024-07-31 16:13:36 +1000

    route

--- mobile/App/src/screens/home/history/History.tsx
+++ mobile/App/src/screens/home/history/History.tsx
@@ -2,16 +2,19 @@ import React, { useState }from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import CustomButton from '../../../components/MISC/Button';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { DrawerActions } from '@react-navigation/native';
+import { DrawerActions, RouteProp } from '@react-navigation/native';
 import { RootStackParamList } from '../../../contexts/types';
 
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>;
+type HistoryRoute = RouteProp<RootStackParamList, 'History'>;
 
 type Props = {
     navigation: HomeScreenNavigationProp;
+    route: HistoryRoute
 };
 
-const History: React.FC<Props> = ({ navigation }) => {
+const History: React.FC<Props> = ({ navigation,route }) => {
+    console.log(route)
     return (
         <View style={styles.container}>
             <Text style={styles.text}>History</Text>
